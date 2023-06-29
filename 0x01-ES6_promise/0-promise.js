9e90aa5c5dc4dae340e28088dc48ad32fd14ac16
Adefemi Oreyomi <olufy007@gmail.com>
2023-06-29 17:21:57 +0100

    My commit

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // API request logic or asynchronous operation here
+    // Call resolve with the desired response when the operation is successful
+    // Call reject with an error if the operation encounters any issues
+  });
+}
---
+++ 0x01-ES6_promise/1-main.js
@@ -0,0 +1,4 @@
+[B[B[B[B[B[Bimport getFullResponseFromAPI from './1-promise';
+
+console.log(getFullResponseFromAPI(true));
+console.log(getFullResponseFromAPI(false));
