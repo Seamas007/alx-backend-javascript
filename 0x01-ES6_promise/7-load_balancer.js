e1a5695d04c7311f44ddbe9b713d38edf112161f
Seamas007 <olufy007@gmail.com>
2023-07-08 14:44:05 +0100

    My commit

--- 0x01-ES6_promise/7-load_balancer.js
+++
@@ -1,4 +0,0 @@
-export default function loadBalancer(chinaDownload, USDownload) {
-  // Return a new promise that settles with the outcome of the first resolving/rejecting promise
-  return Promise.race([chinaDownload, USDownload]);
-}
