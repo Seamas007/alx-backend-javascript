ad17bcf3a515861fa624217ad78136222de727ec
Seamas007 <olufy007@gmail.com>
2023-08-21 16:32:25 +0100

    My commit

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,8 +1,3 @@
-/**
- * Displays a message that print in STDOUT.
- * @param {String} msg The message to display.
- * @author Adefemi Oreyomi <https://github.com/seamas007>
- */
 const displayMessage = (msg) => {
   console.log(msg);
 };
